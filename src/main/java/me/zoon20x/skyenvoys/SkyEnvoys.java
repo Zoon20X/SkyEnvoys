f17927a9d56c254206494cacdd07d15dad6e214b
Zoon20X <47308184+Zoon20X@users.noreply.github.com>
2021-01-18 21:22:21 +1000

    Initial commit

---
+++ src/main/java/me/zoon20x/skyenvoys/SkyEnvoys.java
@@ -0,0 +1,17 @@
+package me.zoon20x.skyenvoys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyEnvoys extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
